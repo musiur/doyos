4f89975b5d2c8a4175b4b9c7f448f23bb230b9b0
musiur <musiur.opu@gmail.com>
2023-10-31 18:29:43 +0600

    [Feature] Homepage: Instrument #9

---
+++ src/components/assets/home/instruments/forex.tsx
@@ -0,0 +1,100 @@
+const Forex = () => {
+  return (
+    <svg
+      width="77"
+      height="77"
+      viewBox="0 0 77 77"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M66.2162 24.585C66.2162 31.5574 60.7111 37.1664 53.9729 37.1664C47.2348 37.1664 41.7297 31.5574 41.7297 24.585C41.7297 17.6127 47.2348 12.0037 53.9729 12.0037C60.7111 12.0037 66.2162 17.6127 66.2162 24.585Z"
+        stroke="#C1282D"
+        strokeWidth="2"
+      />
+      <path
+        d="M69.131 16.03C73.4769 24.8831 70.0018 35.6731 61.369 40.13C52.7363 44.5869 42.2149 41.023 37.869 32.1699C33.523 23.3167 36.9982 12.5268 45.6309 8.0699C54.2637 3.613 64.785 7.17686 69.131 16.03Z"
+        stroke="#091E3B"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeDasharray="8 8"
+      />
+      <path
+        d="M38.3648 14.8841L36.4729 12.4589L40.2567 8.57849L42.6216 10.0336"
+        stroke="#091E3B"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M58.6486 24.585C58.6486 27.2713 56.5316 29.4056 53.9729 29.4056C51.4142 29.4056 49.2973 27.2713 49.2973 24.585C49.2973 21.8988 51.4142 19.7645 53.9729 19.7645C56.5316 19.7645 58.6486 21.8988 58.6486 24.585Z"
+        stroke="#C1282D"
+        strokeWidth="2"
+      />
+      <path
+        d="M49.2432 6.63825V3.72797H55.3919V6.1532"
+        stroke="#091E3B"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M62.4865 8.57849L64.3783 6.63831L69.1081 11.0037L67.2162 12.9439"
+        stroke="#091E3B"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M70.527 19.7346H73.3648V25.5551L70.9999 26.0402"
+        stroke="#091E3B"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M64.3783 38.1663L66.2702 40.1065L70.9999 35.256L68.6351 33.3159"
+        stroke="#091E3B"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M51.1351 42.0467V44.957L58.7027 44.4719L58.2297 41.5616"
+        stroke="#091E3B"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M39.7837 35.256L37.4189 37.6813L42.6216 42.0467L44.9864 40.1065"
+        stroke="#091E3B"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M36.4729 28.9505H33.1621V22.1598H36"
+        stroke="#091E3B"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18.9189 20.7046C18.9189 23.9266 16.3784 26.4953 13.2973 26.4953C10.2162 26.4953 7.67567 23.9266 7.67567 20.7046C7.67567 17.4826 10.2162 14.9139 13.2973 14.9139C16.3784 14.9139 18.9189 17.4826 18.9189 20.7046Z"
+        stroke="#C1282D"
+        strokeWidth="2"
+      />
+      <path
+        d="M2.89189 76H71V42.5318L36.9459 58.5383L2.89189 76ZM1 37.1963V73.5748C2.08547 75.1916 5.02403 77.4552 8.09459 73.5748V55.143L14.2432 54.1729V64.3589C15.3468 65.6524 18.3108 67.4632 21.3378 64.3589V50.7776C21.0225 49.6458 19.1622 47.2852 14.2432 46.8972V35.7411H22.7567L30.3243 29.4355V22.1598L21.3378 29.4355H8.09459C5.72973 30.0822 1 32.5398 1 37.1963Z"
+        stroke="#091E3B"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Forex;
