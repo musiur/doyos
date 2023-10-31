dedec58e6cd085afe887505a0c01fd6ac4244ec2
musiur <musiur.opu@gmail.com>
2023-10-31 20:21:54 +0600

    [Feature] Homepage: Instrument #9

--- src/components/assets/home/instruments/forex.tsx
+++ src/components/assets/home/instruments/forex.tsx
@@ -8,80 +8,6 @@ const Forex = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M66.2162 24.585C66.2162 31.5574 60.7111 37.1664 53.9729 37.1664C47.2348 37.1664 41.7297 31.5574 41.7297 24.585C41.7297 17.6127 47.2348 12.0037 53.9729 12.0037C60.7111 12.0037 66.2162 17.6127 66.2162 24.585Z"
-        stroke="#C1282D"
-        strokeWidth="2"
-      />
-      <path
-        d="M69.131 16.03C73.4769 24.8831 70.0018 35.6731 61.369 40.13C52.7363 44.5869 42.2149 41.023 37.869 32.1699C33.523 23.3167 36.9982 12.5268 45.6309 8.0699C54.2637 3.613 64.785 7.17686 69.131 16.03Z"
-        stroke="#091E3B"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeDasharray="8 8"
-      />
-      <path
-        d="M38.3648 14.8841L36.4729 12.4589L40.2567 8.57849L42.6216 10.0336"
-        stroke="#091E3B"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M58.6486 24.585C58.6486 27.2713 56.5316 29.4056 53.9729 29.4056C51.4142 29.4056 49.2973 27.2713 49.2973 24.585C49.2973 21.8988 51.4142 19.7645 53.9729 19.7645C56.5316 19.7645 58.6486 21.8988 58.6486 24.585Z"
-        stroke="#C1282D"
-        strokeWidth="2"
-      />
-      <path
-        d="M49.2432 6.63825V3.72797H55.3919V6.1532"
-        stroke="#091E3B"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M62.4865 8.57849L64.3783 6.63831L69.1081 11.0037L67.2162 12.9439"
-        stroke="#091E3B"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M70.527 19.7346H73.3648V25.5551L70.9999 26.0402"
-        stroke="#091E3B"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M64.3783 38.1663L66.2702 40.1065L70.9999 35.256L68.6351 33.3159"
-        stroke="#091E3B"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M51.1351 42.0467V44.957L58.7027 44.4719L58.2297 41.5616"
-        stroke="#091E3B"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M39.7837 35.256L37.4189 37.6813L42.6216 42.0467L44.9864 40.1065"
-        stroke="#091E3B"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M36.4729 28.9505H33.1621V22.1598H36"
-        stroke="#091E3B"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
         d="M18.9189 20.7046C18.9189 23.9266 16.3784 26.4953 13.2973 26.4953C10.2162 26.4953 7.67567 23.9266 7.67567 20.7046C7.67567 17.4826 10.2162 14.9139 13.2973 14.9139C16.3784 14.9139 18.9189 17.4826 18.9189 20.7046Z"
         stroke="#C1282D"
         strokeWidth="2"
@@ -93,6 +19,13 @@ const Forex = () => {
         strokeLinecap="round"
         strokeLinejoin="round"
       />
+      <path
+        d="M40 18.5L38 24.5H35V30.5H38L41 36L39 38L44.5 42.5L46 40.5L51.5 42.5V44.5L58 44V42L63.5 39L65 40.5L69 36L67 34.5L69 28H71.5V22.5H69L66 17L67.5 15L63.5 11L61.5 13L55.5 11V9H50V11.5L44 14.5L42 13L38.5 16.5L40 18.5Z"
+        stroke="#091E3B"
+        strokeWidth="2"
+      />
+      <circle cx="54" cy="27" r="10.5" stroke="#C1282D" strokeWidth="2" />
+      <circle cx="54" cy="27" r="3.5" stroke="#091E3B" strokeWidth="2" />
     </svg>
   );
 };
