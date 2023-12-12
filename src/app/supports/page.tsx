e04c33882023982f0560f7d9d996e2421dc1d48a
Irfanul Haque Sajid <irfanhsajid@gmail.com>
2023-12-13 01:46:08 +0600

    created a molecule card and declared folder names

--- src/app/supports/page.tsx
+++ src/app/supports/page.tsx
@@ -1,9 +1,10 @@
-import MostPopularTrade from '@/components/pages/trading-and-tools/forex/most-popular-trade';
+import MostPopularTrade from '@/components/pages/trading-and-tools/market/forex/forex';
 
 const Supports = () => {
     return (
         <>
             <h1>Supports</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, hic sint iste officia fuga provident assumenda repellendus cumque ut asperiores rerum, neque molestias aut sequi cupiditate, ullam eveniet iusto. Nostrum!</p>
             <MostPopularTrade />
         </>
     );
