082b620514c5e715af072ce437a4859cebfe9204
musiur <musiur.opu@gmail.com>
2024-01-09 03:05:54 +0600

    [Enahncement] INDICES asset remove from forex

--- src/app/trading-and-tools/[slug]/[child]/page.tsx
+++ src/app/trading-and-tools/[slug]/[child]/page.tsx
@@ -1,11 +1,9 @@
-import Indices from "@/components/assets/home/instruments/indices";
 import ForexTrading from "@/components/pages/trading-and-tools/market/forex/forex";
 
 const MetaData5 = ({ params }: { params: { child: string } }) => {
   return (
     <div>
       <ForexTrading />
-      <Indices />
     </div>
   );
 };
