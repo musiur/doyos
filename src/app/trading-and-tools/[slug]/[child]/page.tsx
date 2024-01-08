1a38b52700016e18fea9d6e2025248da9f0f1a48
Irfanul Haque Sajid <irfanhsajid@gmail.com>
2024-01-08 22:53:19 +0600

    forex most popular card issue

--- src/app/trading-and-tools/[slug]/[child]/page.tsx
+++ src/app/trading-and-tools/[slug]/[child]/page.tsx
@@ -4,10 +4,11 @@ import ForexTrading from "@/components/pages/trading-and-tools/market/forex/fore
 const MetaData5 = ({ params }: { params: { child: string } }) => {
   console.log(params.child, "<--");
   return (
-    <div>
+    <div className="">
       <ForexTrading />
       <br />
       <Indices />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, nesciunt. Ea quod aperiam autem. Laudantium culpa consequatur perspiciatis tenetur, molestias excepturi dicta. Consequuntur nam asperiores at totam reprehenderit, voluptatem amet!
     </div>
   );
 };
--- src/components/pages/trading-and-tools/market/forex/forex.tsx
+++ src/components/pages/trading-and-tools/market/forex/forex.tsx
@@ -7,6 +7,9 @@ const ForexTrading = () => {
     <div>
       <div className="bg-[url('/images/pages/trading-and-tools/market/ForexTradingBanner.svg')] bg-cover bg-center h-[317px]"></div>
       <br />
+
+
+
       {/* most popular forex trading content card */}
       <MostPopularCard
         sectionImage={
@@ -17,30 +20,13 @@ const ForexTrading = () => {
             height={493}
           />
         }
-        highlighter="Hello"
-        heading={<>Hello</>}
-        paragraphs={[<>Hello</>]}
+
+        heading={<>Trade The Most Popular <span className="h2-span text-secondary">Forex
+          Trading Pairs</span> with Low Spreads</>}
+        paragraphs={[<>Trade Forex with a globally recognized broker. Buy and sell CFDs through our bespoke MetaTrader 5 trading platform, and benefit from the award-winning multilingual customer support available 24/7, <br /> <>Some believe you must choose between an online broker and a wealth management firm. At DOYOS, you don&apos;t need to compromise. Whether you invest on your own, with an advisor, or a little of both — we can support you.</>
+        </>]}
       />
-      {/* <div className="container  flex flex-col-reverse lg:flex-row items-center justify-between py-[70px]">
-                <div className="text">
-                    <h1 className="font-bold text-[27px] md:text-[35px] lg:text-[48px] max-w-[664px] leading-[55px] pt-5" >
-                        Trade The Most Popular <span className="action-span">Forex Trading Pairs
-                        </span> with Low Spreads
-                    </h1>
-                    <p className="max-w-[679px] md:pt-[50px] pb-[22px]">Trade Forex with a globally recognized broker. Buy and sell CFDs through our bespoke MetaTrader 5 trading platform, and benefit from the award-winning multilingual customer support available 24/7.</p>
-                    <p className="max-w-[752px]">
-                        Some believe you must choose between an online broker and a wealth management firm. At DOYOS, you don&apos;t need to compromise. Whether you invest on your own, with an advisor, or a little of both — we can support you.
-                    </p>
-                </div>
-                <div className="image">
-                    <Image
-                        src="/images/pages/trading-and-tools/market/popular-trade.png"
-                        alt="most popular trade image"
-                        width={600}
-                        height={493}
-                    />
-                </div>
-            </div> */}
+
     </div>
   );
 };
